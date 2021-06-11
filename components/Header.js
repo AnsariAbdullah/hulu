8c85aabcbfb4f6bcac0ffedda40f4d1ecf8861e1
AnsariAbdullah <ansariabdullahar@gmail.com>
2021-06-11 17:50:35 +0530

    add header image and styles

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,10 @@
+import Image from 'next/image'
+
 function Header () {
 	return (
 		<header>
-			<h1>This is the header</h1>
+			<h1>Thi is heasder</h1>
+			<Image className='object-contain' src="https://links.papareact.com/ua6" width={200} height={100} />
 		</header>
 	);
 }
